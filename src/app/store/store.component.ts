1e5cc129cd169faf470fb26dfde8b3f8849a0ddb
Mateusz <mateusz.czapla@comarch.com>
2023-04-12 22:50:19 +0200

    Początkowa funkcjonalność koszyka

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Cart } from "../model/cart.model";
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
 
@@ -12,7 +13,7 @@ export class StoreComponent {
     public productsPerPage = 4;
     public selectedPage = 1;
 
-    constructor(private repository: ProductRepository) { }
+    constructor(private repository: ProductRepository, private cart: Cart) { }
 
     get products(): Product[] {
         let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
@@ -40,6 +41,9 @@ export class StoreComponent {
         return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage)
     }
 
+    addProductToCart(product: Product) {
+        this.cart.addLine(product);
+    }
     //get pageNumbers(): number[] {
     //    return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage))
     //        .fill(0).map((x, i) => i + 1);
